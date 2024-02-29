dc31b186ecc5d00b61da2315ff86ecd3b30de416
lucaspagliari <lucas.alex.pagliari@gmail.com>
2024-02-29 17:31:31 -0300

    feat: Add tooling packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import ViteWebfontDownload from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({ 
+      dts: 'src/components.d.ts',
+      dirs: ['src/components']
+    }),
+
+    ViteWebfontDownload(),
   ],
   resolve: {
     alias: {
