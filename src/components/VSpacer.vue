b9f9b0e08e592b25f15d4ba90cfac9b442596215
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-03-03 21:09:57 -0300

    feat: Create VSpacer component

---
+++ src/components/VSpacer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    size: number;
+  }>(),
+  {
+    size: 1,
+  }
+);
+
+const heightMultiplier = 8;
+const style = computed(() => ({
+  height: `${props.size * heightMultiplier}px`,
+}));
+</script>
+
+<template>
+  <div class="v-spacer" :style="style"></div>
+</template>
+
+<style scoped>
+.v-spacer {
+  width: 100%;
+}
+</style>
