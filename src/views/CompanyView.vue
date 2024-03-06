14d4e595ff75799a616295c504ef21956c454215
lucaspagliari <lucas.alexpagliari@gmail.com>
2024-03-05 22:09:51 -0300

    fix(CompanyView): readjust sizes and spaces

--- src/views/CompanyView.vue
+++ src/views/CompanyView.vue
@@ -36,46 +36,61 @@ const plans = [
 </script>
 
 <template>
-  <div class="company">
+  <div class="company-view">
     <VSpacer :size="6" />
 
-    <h1 class="company__title">Tecnologias</h1>
-    <div class="company__technologies">
-      <VTechCarousel :techs="techs" />
+    <div class="company-view__content">
+      <h1 class="company-view__title">Tecnologias</h1>
+      <div class="company-view__technologies">
+        <VTechCarousel :techs="techs" />
+      </div>
     </div>
 
-    <h1 class="company__title">Descobrir</h1>
-    <div class="company__plans">
-      <v-plan-card v-for="plan in plans" v-bind="plan">
-        <template #action>
-          <v-button color="green" text-color="white">
-            Consultar cobertura
-          </v-button>
-        </template>
-      </v-plan-card>
+    <VSpacer :size="5" />
+
+    <div class="company-view__content">
+      <h1 class="company-view__title">Descobrir</h1>
+      <div class="company-view__plans">
+        <v-plan-card v-for="plan in plans" v-bind="plan">
+          <template #action>
+            <v-button color="green" text-color="white">
+              Consultar cobertura
+            </v-button>
+          </template>
+        </v-plan-card>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.company {
-  &__technologies {
+.company-view {
+  $self: &;
+
+  &__content {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  &__title {
-    margin: 0 15%;
-  }
+    gap: 20px;
 
-  &__plans {
-    display: grid;
-    align-items: center;
-    justify-content: center;
 
-    grid-template-columns: repeat(3, auto);
-    gap: 18px;
+    #{$self}__technologies {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    #{$self}__title {
+      margin: 0 15%;
+    }
+
+    #{$self}__plans {
+      display: grid;
+      align-items: center;
+      justify-content: center;
+
+      grid-template-columns: repeat(3, auto);
+      gap: 18px;
+    }
   }
 }
 </style>
